1a7e4e028be1851f1c02f7771994d6473e4fe835
AnisDos <41198406+AnisDos@users.noreply.github.com>
2024-08-28 17:44:01 +0100

    change date

--- date-time.js
+++ date-time.js
@@ -1,7 +1,7 @@
 (function(){
             // new version 
-            var due_date = new Date('2024-08-13');
-            var days_deadline = 60;
+            var due_date = new Date('2024-09-02');
+            var days_deadline = 1;
             var current_date = new Date();
             var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
             var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
