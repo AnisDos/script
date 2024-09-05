0b2b92f936cedb3d5725601c4a7d8ca0165b5cfb
AnisDos <41198406+AnisDos@users.noreply.github.com>
2024-09-05 11:52:40 +0100

    Update date-time.js

--- date-time.js
+++ date-time.js
@@ -1,6 +1,6 @@
 (function(){
             // new version 
-            var due_date = new Date('2024-08-30');
+            var due_date = new Date('2028-08-30');
             var days_deadline = 1;
             var current_date = new Date();
             var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
